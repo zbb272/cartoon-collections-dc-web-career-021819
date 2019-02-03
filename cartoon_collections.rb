142f1ff3501b49be39e1a987ce9556ba45af9aa8
Zachary <zbberman@yahoo.com>
2019-02-03 14:25:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do | dwarf, index |
+    puts "#{index}. #{dwarf}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(words)
+  words.collect do | word |
+    word.capitalize!
+    word << "!"
+  end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.collect do | word | 
+    if word.size > 4
+      return true 
+    end 
+  end
+  false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+describe "#find_the_cheese" do
+    it "returns the first element of the array that is cheese" do
+      contains_cheddar = ["banana", "cheddar", "sock"]
+      expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
+
+      contains_gouda = ["potato", "gouda", "camembert"]
+      expect(find_the_cheese(contains_gouda)).to eq 'gouda'
+    end
+
+    it "returns nil if the array does not contain a type of cheese" do
+      no_cheese = ["ham", "cellphone", "computer"]
+      expect(find_the_cheese(no_cheese)).to eq nil
+    end
+  end
