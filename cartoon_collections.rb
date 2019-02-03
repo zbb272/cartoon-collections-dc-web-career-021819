6ec1b0ed59502f174bd3b9b40c626b246dcf4911
Zachary <zbberman@yahoo.com>
2019-02-03 14:25:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,18 +33,3 @@ def find_the_cheese(array)
     array.find {|x| x == cheese}
   end
 end
-
-describe "#find_the_cheese" do
-    it "returns the first element of the array that is cheese" do
-      contains_cheddar = ["banana", "cheddar", "sock"]
-      expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
-
-      contains_gouda = ["potato", "gouda", "camembert"]
-      expect(find_the_cheese(contains_gouda)).to eq 'gouda'
-    end
-
-    it "returns nil if the array does not contain a type of cheese" do
-      no_cheese = ["ham", "cellphone", "computer"]
-      expect(find_the_cheese(no_cheese)).to eq nil
-    end
-  end
